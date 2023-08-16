eda1b3f1bd191868a4b35aeb7693344ff5523b2e
Mahedi <faruk585@gmail.com>
2023-08-16 16:34:57 +0600

    6 problem solved

---
+++ Q_3/Q_3.js
@@ -0,0 +1,10 @@
+
+
+// Add a class "selected" to all <li> elements within an unordered list.
+
+const list = document.getElementsByTagName("li");
+
+for (let allList of list) {
+  allList.classList.add("selected");
+  
+}

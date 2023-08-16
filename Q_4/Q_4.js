2b68428026e8a4ad62c8d8ccec993e35cb8f05a8
Mahedi <faruk585@gmail.com>
2023-08-16 16:05:41 +0600

    5 problem solved

---
+++ Q_4/Q_4.js
@@ -0,0 +1,7 @@
+
+
+// Change the font size of the first paragraph within a <div> with the class "content" to 18px.
+
+const firsrPara = document.querySelector(".content");
+
+firsrPara.style.fontSize = '18px';
